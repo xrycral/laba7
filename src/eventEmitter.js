717c5c3d910f4c677a72577fc2a8cb32bfd7409d
xrycral <dimapyrig645@gmail.com>
2026-05-05 16:16:00 +0300

    add SafeEventEmitter skeleton

---
+++ src/eventEmitter.js
@@ -0,0 +1,37 @@
+class SafeEventEmitter {
+  constructor() {
+    this._listeners = {};
+  }
+
+  on(event, listener) {
+    if (!this._listeners[event]) {
+      this._listeners[event] = [];
+    }
+    this._listeners[event].push(listener);
+    return () => this.off(event, listener);
+  }
+
+  off(event, listener) {
+    if (!this._listeners[event]) return;
+    this._listeners[event] = this._listeners[event].filter(
+      (l) => l === listener
+    );
+  }
+
+  emit(event, ...args) {
+    const listeners = this._listeners[event] || [];
+    for (const listener of listeners) {
+      listener(...args);
+    }
+  }
+
+  removeAllListeners(event) {
+    if (event) {
+      delete this._listeners[event];
+    } else {
+      this._listeners = {};
+    }
+  }
+}
+
+export { SafeEventEmitter };
