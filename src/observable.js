d47e0c3976b252ff0b05bd8d6a47716d93047f2c
xrycral <dimapyrig645@gmail.com>
2026-05-05 17:43:02 +0300

    add Observable with subscribe and unsubscribe

---
+++ src/observable.js
@@ -0,0 +1,50 @@
+class Observable {
+  constructor(subscribeFn) {
+    this._subscribeFn = subscribeFn;
+  }
+
+  subscribe({ onNext, onError, onComplete }) {
+    let isUnsubscribed = false;
+
+    const observer = {
+      next(value) {
+        if (!isUnsubscribed && onNext) onNext(value);
+      },
+      error(err) {
+        if (!isUnsubscribed && onError) onError(err);
+      },
+      complete() {
+        if (!isUnsubscribed && onComplete) onComplete();
+      },
+    };
+
+    const cleanup = this._subscribeFn(observer);
+
+    return {
+      unsubscribe() {
+        if (cleanup) cleanup();
+      },
+    };
+  }
+
+  static from(values) {
+    return new Observable((observer) => {
+      for (const value of values) {
+        observer.next(value);
+      }
+      observer.complete();
+    });
+  }
+
+  static interval(ms) {
+    return new Observable((observer) => {
+      let count = 0;
+      const timer = setInterval(() => {
+        observer.next(count++);
+      }, ms);
+      return () => clearInterval(timer);
+    });
+  }
+}
+
+export { Observable };
