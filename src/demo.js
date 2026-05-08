be230be05440d8fbc2bad1986cca055880566fda
xrycral <dimapyrig645@gmail.com>
2026-05-08 14:27:25 +0300

    add observable demos and fix missing import

--- src/demo.js
+++ src/demo.js
@@ -1,4 +1,5 @@
 import { SafeEventEmitter } from './eventEmitter.js';
+import { Observable } from './observable.js';
 
 function demoMultipleListeners() {
   console.log('=== Demo 1: Multiple Listeners ===\n');
@@ -34,4 +35,54 @@ function demoMultipleListeners() {
   emitter.emit('message', 'Second message');
 }
 
+function demoErrorChannel() {
+  console.log('\n=== Demo 2: Error Channel ===\n');
+
+  const emitter = new SafeEventEmitter();
+
+  emitter.on('data', () => {
+    throw new Error('something broke in data handler');
+  });
+
+  emitter.on('error', (err, src) => {
+    console.error(`[ErrorChannel] from "${src}": ${err.message}`);
+  });
+
+  emitter.emit('data', 42);
+}
+
+function demoObservable() {
+  console.log('\n=== Demo 3: Observable ===\n');
+
+  const nums = Observable.from([1, 2, 3, 4, 5]);
+
+  nums.subscribe({
+    onNext: (v) => console.log(`[Sub1] value: ${v}`),
+    onComplete: () => console.log('[Sub1] done'),
+  });
+
+  nums.subscribe({
+    onNext: (v) => console.log(`[Sub2] squared: ${v * v}`),
+    onComplete: () => console.log('[Sub2] done'),
+  });
+}
+
+function demoInterval() {
+  console.log('\n=== Demo 4: Interval + Unsubscribe ===\n');
+
+  const ticker = Observable.interval(100);
+
+  const sub = ticker.subscribe({
+    onNext: (tick) => console.log(`[Ticker] tick: ${tick}`),
+  });
+
+  setTimeout(() => {
+    sub.unsubscribe();
+    console.log('[Ticker] stopped');
+  }, 350);
+}
+
 demoMultipleListeners();
+demoErrorChannel();
+demoObservable();
+demoInterval();
