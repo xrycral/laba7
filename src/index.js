eee2d55555d5c264679ff3542667932a041145f7
xrycral <dimapyrig645@gmail.com>
2026-05-08 14:43:24 +0300

    add entry point

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { SafeEventEmitter } from './eventEmitter.js';
+import { Observable } from './observable.js';
+
+console.log('=== Lab 7: Reactive Communication ===');
+console.log(`running: ${SafeEventEmitter.name}, ${Observable.name}`);
+
+import('./demo.js');
